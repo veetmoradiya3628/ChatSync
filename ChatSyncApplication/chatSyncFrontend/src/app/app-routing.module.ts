42d13202be9f052bc04a81c8f1d24efbeff3134a
veetmoradiya <veetmoradiya3628@gmail.com>
2024-02-03 12:07:07 +0530

    basic layout and pages/components for frontend app added

--- ChatSyncApplication/chatSyncFrontend/src/app/app-routing.module.ts
+++ ChatSyncApplication/chatSyncFrontend/src/app/app-routing.module.ts
@@ -1,11 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ChatsComponent } from './chat/chats/chats.component';
+import { GroupsComponent } from './group/groups/groups.component';
+import { ContactsComponent } from './contact/contacts/contacts.component';
+import { ProfileComponent } from './common/profile/profile.component';
+import { LogoutComponent } from './common/logout/logout.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AppComponent
+    redirectTo: '/chat',
+    pathMatch: 'full'
+  },
+  {
+    path: 'chat',
+    component: ChatsComponent,
+    children: []
+  },
+  {
+    path: 'group',
+    component: GroupsComponent,
+    children: []
+  },
+  {
+    path: 'contact',
+    component: ContactsComponent,
+    children: []
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    children: []
+  },
+  {
+    path: 'logout',
+    component: LogoutComponent,
+    children: []
   }
 ];
 
--- ChatSyncApplication/chatSyncFrontend/src/app/app.module.ts
+++ ChatSyncApplication/chatSyncFrontend/src/app/app.module.ts
@@ -4,15 +4,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavHeaderComponent } from './common/nav-header/nav-header.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { ChatsComponent } from './chat/chats/chats.component';
+import { ContactsComponent } from './contact/contacts/contacts.component';
+import { GroupsComponent } from './group/groups/groups.component';
+import { ProfileComponent } from './common/profile/profile.component';
+import { LogoutComponent } from './common/logout/logout.component';
+import { LoginComponent } from './common/login/login.component';
+import { RegisterComponent } from './common/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavHeaderComponent,
+    ChatsComponent,
+    ContactsComponent,
+    GroupsComponent,
+    ProfileComponent,
+    LogoutComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
